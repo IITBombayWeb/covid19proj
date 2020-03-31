4346ddd64c1df62fa0df6923063f281b7e4f7189
P Sunthar <p.sunthar@sankhyasutralabs.com>
2020-03-31 23:52:24 +0530

    add three lists

--- inest.js
+++ inest.js
@@ -18,10 +18,27 @@ document.getElementById("demo").innerHTML = "Hello JavaScript";
 
 
 var ilist = [
-    new Inventory("Vents", function(n){ return n*10;}),
-    new Inventory("Sanitizer", function(n){ return n*20;}, " lt / day"),
-    new Inventory("Gowns", function(n){ return n*7;}),
-    new Inventory("Oxygen",function(n){ return n*5;}, "cylinders / day")
+    //PPE: Persons and Protective Equipment
+    new Inventory("Doctors", function(n){ return Math.ceil(n/10*4);},"per day"),
+    new Inventory("Nurses", function(n){ return n;},"per day"),
+    new Inventory("Gowns, Splash Guard", function(n){ return Math.ceil(n*14/5);},"per day"),
+    new Inventory("Masks, Gloves", function(n){ return 70*n;}, "per day"),
+
+    //Medical Equipment
+    new Inventory("Ventilators", function(n){ return n;}),
+    new Inventory("ET Tube", function(n){ return n;}),
+    new Inventory("Laryngoscopes", function(n){ return Math.ceil(n/20);}),
+    new Inventory("Ambu Bags", function(n){ return n;}),
+    new Inventory("Glass case", function(n){ return n;}),
+    new Inventory("Infusion pump", function(n){ return n;}),
+    
+
+    //Medical Consumables
+    new Inventory("Sanitizer", function(n){ return Math.ceil(n*14/10000);}, " lt / day"),
+    new Inventory("Oxygen (medium)",function(n){ return n*4;}, "cylinders / day"),
+    new Inventory("Central, Peripheral lines",function(n){ return Math.ceil(n/3);}, "per day"),
+    new Inventory("IV sugar, saline",function(n){ return Math.ceil(n*2*500/1000);}, "l / day"),
+    new Inventory("Suction catheter",function(n){ return n;}),
 ];
 
     
