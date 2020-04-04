dbaeff2a2afad978de5f74b40cc73a10bafeff0b
mayank <mayankmodi.2011@gmail.com>
2020-04-04 20:26:52 +0530

    bug fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PredectionsComponent } from './predections/predections.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatCardModule} from '@angular/material/card';
 import {PredectionService} from './predection.service'
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatTableModule} from '@angular/material/table';
-import {MatSelectModule} from '@angular/material/select';
-import {MatListModule} from '@angular/material/list';
-import {MatDividerModule} from '@angular/material/divider';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AboutComponent } from './about/about.component';
 import { FaqsComponent } from './faqs/faqs.component';
@@ -31,16 +22,7 @@ import { NotificationComponent } from './notification/notification.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    MatCardModule,
     HttpClientModule,
-    MatToolbarModule,
-    MatTooltipModule,
-    MatGridListModule,
-    MatTableModule,
-    MatSelectModule,
-    MatListModule,
-    MatDividerModule
   ],
   providers: [PredectionService],
   bootstrap: [AppComponent]
